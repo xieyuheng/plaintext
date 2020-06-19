cd581cbab2dac275e2e4c2ccd0a5efa2fcb8ea0b
Xie Yuheng <xyheme@gmail.com>
2020-06-19 22:07:08 +0800

    up

--- src/cli/cli-build.ts
+++ src/cli/cli-build.ts
@@ -1,5 +1,22 @@
+import * as pln from "../api"
+import path from "path"
 import fs from "fs"
+import readdir_rec from "fs-readdir-recursive"
 
-export function run(file: string, opts: any): void {
-  console.log("><><><")
+export function run(dir: string, opts: any): void {
+  const out = path.resolve(opts.out || "out")
+  const files = readdir_rec(dir)
+  for (const file of files) {
+    let text = fs.readFileSync(path.resolve(dir, file), { encoding: "utf-8" })
+    text = pln.escape_for_xml(text)
+    text = pln.html_wrapper(text)
+    const output_file = path.resolve(out, file)
+    const output_dir = path.dirname(output_file)
+    fs.mkdirSync(output_dir, { recursive: true })
+    fs.writeFile(output_file + ".html", text, (error) => {
+      if (error) {
+        console.log(error)
+      }
+    })
+  }
 }
--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -3,9 +3,12 @@ import commander from "commander"
 export function run(config: any): void {
   const program = new commander.Command()
 
-  program.name("ptl").version(config.version, "-v, --version")
+  program.name("plaintext").version(config.version, "-v, --version")
 
-  program.command("build <input-directory>").action(require("./cli-build").run)
+  program
+    .command("build <input-directory>")
+    .option("-o, --out <output-directory>")
+    .action(require("./cli-build").run)
 
   program.parse(process.argv)
 }
