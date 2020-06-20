2fbe3a48c9ac5b49024132891f8e14392da8ec31
Xie Yuheng <xyheme@gmail.com>
2020-06-20 20:59:08 +0800

    - top level command instead of `plaintext build`

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,17 +1,18 @@
 import commander from "commander"
+import * as cli_build from "./cli-build"
 
 export function run(config: any): void {
   const program = new commander.Command()
 
-  program.name("plaintext").version(config.version, "-v, --version")
-
   program
-    .command("build <input-directory>")
+    .name("plaintext")
+    .version(config.version, "-v, --version")
+    .arguments("<input-directory>")
     .requiredOption("-o, --output <output-directory>")
     .option("--verbose")
     .option("--title <title-of-index-page>")
     .option("--prolog <prolog-of-index-page>")
-    .action(require("./cli-build").run)
+    .action(cli_build.run)
 
   program.parse(process.argv)
 }
