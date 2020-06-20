edd7d7c9d9ba87598ee23c67497744ca22f47647
Xie Yuheng <xyheme@gmail.com>
2020-06-20 21:33:21 +0800

    up

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
 export * from "./api/pln-escape-for-xml"
-export * as ut from "./ut"
--- src/main.ts
+++
@@ -1,5 +0,0 @@
-export * from "./index"
-// To import this library,
-//   we suggest you to use the following prefix:
-import * as pln from "./index"
-export default pln
