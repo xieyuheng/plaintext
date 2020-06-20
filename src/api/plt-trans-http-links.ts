d86b89bbd4c4d1528b7c45711917bb4179348598
Xie Yuheng <xyheme@gmail.com>
2020-06-20 14:20:31 +0800

    also abs path

--- src/api/plt-trans-http-links.ts
+++ src/api/plt-trans-http-links.ts
@@ -1,6 +1,5 @@
 export function trans_http_links(text: string): string {
-  const regexp = /(\s)(https?:\/\/\S*)/gu
-  return text.replace(regexp, (target, space, link) => {
+  return text.replace(/(\s)(https?:\/\/\S*)/gu, (target, space, link) => {
     return space + `<a href="${link}">${link}</a>`
   })
 }
--- src/api/plt-trans-local-links.ts
+++ src/api/plt-trans-local-links.ts
@@ -2,10 +2,10 @@
 // - example link:./plt-trans-http-links.ts
 // - example link:../index.ts
 // - example link:../../index
+// - example link:/index
 
 export function trans_local_links(text: string): string {
-  const regexp = /(\s)(link:)(\.\.?\S*)/gu
-  return text.replace(regexp, (target, space, scheme, link) => {
+  return text.replace(/(\s)(link:)(\/\S*|\.\.?\S*)/gu, (target, space, scheme, link) => {
     return space + `<a href="${`${link}.html`}">${scheme + link}</a>`
   })
 }
